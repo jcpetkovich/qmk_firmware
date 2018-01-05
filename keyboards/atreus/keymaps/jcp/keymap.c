774072e8d3f66065b156087d520c76204ad946c6
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2018-01-04 23:58:04 -0500

    Updating a few layers.

--- keyboards/atreus/keymaps/jcp/keymap.c
+++ keyboards/atreus/keymaps/jcp/keymap.c
@@ -18,7 +18,8 @@ enum atreus_layers {
 	_LOWER,
 	_RAISE,
 	_ADJUST,
-	_NAVIG
+	_NUMBERS,
+	_SYMBOLS
 };
 
 enum planck_keycodes {
@@ -26,9 +27,13 @@ enum planck_keycodes {
 	LOWER,
 	RAISE,
 	ADJUST,
-	NAVIG,
 	BACKLIT
 };
+
+
+// Layers
+#define NUMBERS LT(_NUMBERS, KC_X)
+#define SYMBOLS LT(_SYMBOLS, KC_DOT)
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -75,58 +80,88 @@ enum planck_keycodes {
 #define GUI_FLO HYPR(KC_F)
 #define GUI_TIL HYPR(KC_T)
 
+#define GUI_LFT LGUI(KC_H)
+#define GUI_RGT LGUI(KC_L)
+#define GUI_UP  LGUI(KC_K)
+#define GUI_DWN LGUI(KC_J)
 
+// desktops
+#define GUI_D1  LGUI(KC_1)
+#define GUI_D2  LGUI(KC_2)
+#define GUI_D3  LGUI(KC_3)
+#define GUI_D4  LGUI(KC_4)
+#define GUI_D5  LGUI(KC_5)
+#define GUI_D6  LGUI(KC_6)
+#define GUI_D7  LGUI(KC_7)
+#define GUI_D8  LGUI(KC_8)
+#define GUI_D9  LGUI(KC_9)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Colemak Layer
    ,----------------------------------.              ,----------------------------------.
-   |   Q  |   W  |   F  |   P  |   G  |              |   J  |   L  |   U  |   Y  |   L  |
+   |   Q  |   W  |   F  |   P  |   G  |              |   J  |   L  |   U  |   Y  |   ;  |
    |------+------+------+------+------|              |------+------+------+------+------|
    |   A  |   R  |   S  |   T  |   D  |              |   H  |   N  |   E  |   I  |   O  |
    |------+------+------+------+------|------.,------|------+------+------+------+------|
-   |SFT/ Z|   X  |   C  |   V  |   B  | LOWER||RAISE |   K  |   M  |   ,  |   .  |SFT/ /|
+   |SFT/ Z|NUM/X |   C  |   V  |   B  | LOWER||RAISE |   K  |   M  |   ,  |SYM/. |SFT/ /|
    |------+------+------+------+------|      ||      |------+------+------+------+------|
    |  Esc | HYPR |  GUI | Alt  | SPC  |------'`------| BkSp | Ctrl | GUI/-|   '  | Enter|
    `----------------------------------'              `----------------------------------'*/
 	[_COLEMAK] = {
 		{KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    XXXXXXX, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN},
 		{KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    XXXXXXX, KC_H,    KC_N,    KC_E,    KC_I,    KC_O   },
-		{SFT_ZED, KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   KC_K,    KC_M,    KC_COMM, KC_DOT,  SFT_SLS},
+		{SFT_ZED, NUMBERS, KC_C,    KC_V,    KC_B,    LOWER,   KC_K,    KC_M,    KC_COMM, SYMBOLS, SFT_SLS},
 		{KC_ESC,  KC_HYPR, KC_LGUI, ALT_TAB, KC_SPC,  RAISE,   KC_BSPC, KC_LCTL, GUI_MIN, KC_QUOT, KC_ENT }
 	},
 
-/* LOWER Layer (number oriented)
+/* LOWER Layer (editing/desktop oriented)
+
+   Thumb-triggered layers are great for single press keys and keys you don't
+   press with modifiers, this includes macros that are frequently used (like
+   switching desktops) that could be made significantly more efficient on small
+   layouts like this by eliminating the need to press SUPER.
+
+   This layer takes care of arrow keys and editing tricks, simple cursor
+   navigation and desktop switching (for now). Does it make sense to kep desktop
+   navigation here? It seems out of place somehow, but it's an open layer
+   otherwise.
+
    ,----------------------------------.              ,----------------------------------.
-   | DWORD| BWORD|  UP  | FWORD| Home |              |      |   7  |   8  |   9  |   /  |
+   | DWORD| BWORD|  UP  | FWORD| Home |              |      | Dsk7 | Dsk8 | Dsk9 |      |
    |------+------+------+------+------|              |------+------+------+------+------|
-   | DEL  | Left | Down | Right| End  |              |      |   4  |   5  |   6  |   *  |
+   | DEL  | Left | Down | Right| End  |              | PgUp | Dsk4 | Dsk5 | Dsk6 |      |
    |------+------+------+------+------|------.,------|------+------+------+------+------|
-   |      |      |      | ALTB |   ,  | ON   ||      |      |   1  |   2  |   3  |   -  |
+   |      |      | ALTTB|   ,  |      | ON   ||      | PgDn | Dsk1 | Dsk2 | Dsk3 |      |
    |------+------+------+------+------|      ||      |------+------+------+------+------|
-   |      |      |      |      |      |------'`------|      | CTL/0| GUI/.| CTL/=|   +  |
+   |      |      |      |      |      |------'`------| Ins  | Ctrl | GUI  | Alt  |      |
    `----------------------------------'              `----------------------------------'*/
 	[_LOWER] = {
-		{DWORD,   BWORD,   KC_UP,   FWORD,   KC_HOME, XXXXXXX, _______, KC_7,    KC_8,    KC_9,    KC_SLSH},
-		{KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX, _______, KC_4,    KC_5,    KC_6,    KC_ASTR},
-		{_______, _______, _______, ALTTAB,  KC_COMM, _______, _______, KC_1,    KC_2,    KC_3,    SFT_MIN},
-		{_______, _______, _______, _______, _______, _______, _______, CTL_ZER, GUI_DOT, ALT_EQL, KC_PLUS}
+		{DWORD,   BWORD,   KC_UP,   FWORD,   KC_HOME, XXXXXXX, _______, GUI_D7,  GUI_D8,  GUI_D9,  _______},
+		{KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX, KC_PGUP, GUI_D4,  GUI_D5,  GUI_D6,  _______},
+		{_______, _______, ALTTAB,  KC_COMM, _______, _______, KC_PGDN, GUI_D1,  GUI_D2,  GUI_D3,  _______},
+		{_______, _______, _______, _______, _______, _______, KC_INS,  KC_LCTL, KC_LGUI, KC_LALT, _______}
 	},
 
 /* RAISE Layer (symbol oriented)
+
+   The raise layer is for symbols, but that's somewhat unfortunate as we already
+   have a layer for symbols. This could end up being a "hyperspace" layer like I
+   was planning for the tri-layer.
+
    ,----------------------------------.              ,----------------------------------.
-   |   !  |   @  |   #  |   $  |   %  |              |   ^  |   &  |   *  |   (  |   )  |
+   |      |      |  MWU |      |      |              |      |  WNW |  WN  |  WNE |      |
    |------+------+------+------+------|              |------+------+------+------+------|
-   |   ~  |   [  |   ]  |   {  |   }  |              | PgDn |   [  |   ]  |   {  |   }  |
+   |      |  MWL |  MWD |  MWR |      |              |      |  WW  |  WC  |  WE  |      |
    |------+------+------+------+------|------.,------|------+------+------+------+------|
-   |   `  |   (  |   )  |      |   |  |      || ON   | PgUp |      |   <  |   >  |SFT/ \|
+   |      |      |      |      |      |      || ON   |      |  WSW |  WS  |  WSE |      |
    |------+------+------+------+------|      ||      |------+------+------+------+------|
-   |      | CAPS |      |      |      |------'`------| Ins  |      |      |      |      |
+   |      |      |      |      |      |------'`------|      |      |      |      |      |
    `----------------------------------'              `----------------------------------'*/
 	[_RAISE] = {
-		{KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
-		{KC_TILD, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX, KC_PGUP, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR},
-		{KC_GRV,  KC_LPRN, KC_RPRN, _______, KC_PIPE, _______, KC_PGDN, _______, KC_LABK, KC_RABK, SFT_BSL},
-		{_______, KC_CAPS, _______, _______, _______, _______, KC_INS,  _______, _______, _______, _______}
+		{_______, _______, GUI_UP,  _______, _______, XXXXXXX, _______, GUI_NWE, GUI_NOR, GUI_NES, _______},
+		{_______, GUI_LFT, GUI_DWN, GUI_RGT, _______, XXXXXXX, _______, GUI_WES, _______, GUI_EAS, _______},
+		{_______, _______, _______, _______, _______, _______, _______, GUI_SWE, GUI_SOU, GUI_SES, _______},
+		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 	},
 
 /* ADJUST Layer
@@ -146,23 +181,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET  }
 	},
 
-/* NAVIG (navigation) Layer
+/* NUMBERS Layer
    ,----------------------------------.              ,----------------------------------.
-   |      |      |  MWU |      |      |              |      |  WNW |  WN  |  WNE |      |
+   |      |      |      |      |      |              |      |   7  |   8  |   9  |   /  |
    |------+------+------+------+------|              |------+------+------+------+------|
-   |      |  MWL |  MWD |  MWR |      |              |      |  WW  |  WC  |  WE  |      |
+   |      |      |      |      |      |              |      |   4  |   5  |   6  |   *  |
    |------+------+------+------+------|------.,------|------+------+------+------+------|
-   |      |      |      |      |      |      ||      |      |  WSW |  WS  |  WSE |      |
+   |      | (ON) | ALTB |    , |      |      ||      |      |   1  |   2  |   3  |   -  |
    |------+------+------+------+------|      ||      |------+------+------+------+------|
-   |      |      |      |      |      |------'`------|      |      |      |      |      |
+   |      |      |      |      |      |------'`------|      | CTL/0| GUI/.| ALT/=|   +  |
    `----------------------------------'              `----------------------------------'*/
-	[_NAVIG] = {
-		{KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10 },
-		{KC_F11,  _______, _______, _______, _______, XXXXXXX, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_F12 },
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-		{_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET  }
-	}
+	[_NUMBERS] = {
+		{_______, _______, _______, _______, _______, XXXXXXX, _______, KC_7,    KC_8,    KC_9,    KC_SLSH},
+		{_______, _______, _______, _______, _______, XXXXXXX, _______, KC_4,    KC_5,    KC_6,    KC_ASTR},
+		{_______, _______, ALTTAB,  KC_COMM, _______, _______, _______, KC_1,    KC_2,    KC_3,    SFT_MIN},
+		{_______, _______, _______, _______, _______, _______, _______, CTL_ZER, GUI_DOT, ALT_EQL, KC_PLUS}
+	},
 
+/* SYMBOLS Layer
+   ,----------------------------------.              ,----------------------------------.
+   |   !  |   @  |   #  |   $  |   %  |              |   ^  |   &  |   *  |   (  |   )  |
+   |------+------+------+------+------|              |------+------+------+------+------|
+   |   ~  |   [  |   ]  |   {  |   }  |              |      |      |      |      |      |
+   |------+------+------+------+------|------.,------|------+------+------+------+------|
+   |   `  |   (  |   )  |      |   |  |      ||      |      |   <  |   >  | (ON) |SFT/ \|
+   |------+------+------+------+------|      ||      |------+------+------+------+------|
+   |      | CAPS |      |      |      |------'`------|      |      |      |      |      |
+   `----------------------------------'              `----------------------------------'*/
+	[_SYMBOLS] = {
+		{KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
+		{KC_TILD, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX, _______, _______, _______, _______, _______},
+		{KC_GRV,  KC_LPRN, KC_RPRN, _______, KC_PIPE, _______, _______, KC_LABK, KC_RABK, _______, SFT_BSL},
+		{_______, KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+	}
 };
 
 const uint16_t PROGMEM fn_actions[] = {
--- keyboards/planck/keymaps/jcp/keymap.c
+++ keyboards/planck/keymaps/jcp/keymap.c
@@ -17,7 +17,10 @@
 #include "planck.h"
 #include "action_layer.h"
 
-#define MK_RSEN MT(MOD_RSFT, KC_ENT)
+// Mod-Tap keys, toplevel
+#define SFT_ENT MT(MOD_RSFT, KC_ENT)
+#define CTL_LEF MT(MOD_CTRL, KC_LEFT)
+
 
 extern keymap_config_t keymap_config;
 
@@ -58,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MK_RSEN},
-  {BACKLIT, KC_HYPR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LCTL, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
+  {BACKLIT, KC_HYPR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   CTL_LEF, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Colemak
@@ -70,14 +73,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Hyper| GUI  | Alt  |Lower | Space| Ctrl |Raise | Left | Down |  Up  |Right |
+ * | Brite| Hyper| GUI  | Alt  |Lower | Space| Space|Raise |CTL/LE| Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
   {KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MK_RSEN},
-  {BACKLIT, KC_HYPR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LCTL, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT},
+  {BACKLIT, KC_HYPR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   CTL_LEF, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Dvorak
